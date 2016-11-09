4b90cabd4ece58d9541b9e0d3c9b21868971eca6
Benjamin <benjamin.v.renterghem@gmail.com>
2016-11-09 23:20:32 +0100

    Revert "no message"
    This reverts commit 7c81eabefd6e0a888b21e2f32b7396a75a4d4b35.

--- core/src/be/howest/junglewars/models/missiles/Missile.java
+++ core/src/be/howest/junglewars/models/missiles/Missile.java
@@ -1,28 +1,24 @@
 package be.howest.junglewars.models.missiles;
 
 import be.howest.junglewars.models.*;
-import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.*;
-import com.badlogic.gdx.graphics.g2d.*;
-import com.badlogic.gdx.math.*;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 
 public class Missile extends Model {
 
     private Player owner;
 
     private int damage;
-
     private float rotationSpeed;
-
-    private float dx;
-    private float dy;
 
     private float lifeTime;
     private float lifeTimer;
 
     private boolean remove;
 
-<<<<<<< 552ae2107adaa47f901d65aea84562151788e1c4
     private float radians;
     private float destinationX;
     private float destinationY;
@@ -37,30 +33,18 @@ public class Missile extends Model {
         this.posX = playerX;
         this.posY = playerY;
         this.owner = owner;
-=======
-
-    public Missile(float startX, float startY, float radians) {
-        x = startX;
-        y = startY;
->>>>>>> shoot animation & shoot direction fix
 
         texture = new Texture(Gdx.files.internal("missile/Banana.png"));
         sprite = new Sprite(texture);
-        sprite.setSize(sprite.getWidth() * 0.5f, sprite.getHeight() * 0.5f);
-
+        sprite.setSize(sprite.getWidth()*0.5f, sprite.getHeight()*0.5f);
         damage = 10;
-<<<<<<< 552ae2107adaa47f901d65aea84562151788e1c4
         speed = 10;
         rotationSpeed = 5;
         lifeTime = 0.5f;
         lifeTimer = 0;
-=======
->>>>>>> shoot animation & shoot direction fix
 
-        speed = 300;
-        rotationSpeed = -10;
+        radians = MathUtils.PI/2;
 
-<<<<<<< 552ae2107adaa47f901d65aea84562151788e1c4
         dx = MathUtils.cos(radians/2) * (destinationX - playerX) * speed;
         dy = MathUtils.sin(radians/2) * (destinationY - playerY) * speed;
     }
@@ -71,37 +55,15 @@ public class Missile extends Model {
 
         lifeTimer += dt;
         if (lifeTimer > lifeTime){
-=======
-        dx = MathUtils.cos(radians) * speed;
-        dy = MathUtils.sin(radians) * speed;
-
-        lifeTime = 3;
-        lifeTimer = 0;
-    }
-
-    public void update(float dt) {
-        x += dx * dt;
-        y += dy * dt;
-
-        lifeTimer += dt;
-        if (lifeTimer > lifeTime) {
->>>>>>> shoot animation & shoot direction fix
             remove = true;
         }
     }
 
-    public void render(SpriteBatch batch) {
+    public void render(SpriteBatch batch){
         sprite.setOriginCenter();
-<<<<<<< 552ae2107adaa47f901d65aea84562151788e1c4
         sprite.setPosition(posX - sprite.getWidth()/2, posY - sprite.getHeight()/2);
-=======
-        sprite.setPosition(x - width / 2, y - height / 2);
-        sprite.rotate(rotationSpeed);
->>>>>>> shoot animation & shoot direction fix
         sprite.draw(batch);
     }
 
-    public boolean shouldRemove() {
-        return remove;
-    }
+    public boolean shouldRemove(){ return remove;}
 }
