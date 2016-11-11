59aaa72a40716e81b9f37ce4e969696d6b0654eb
Benjamin Van Renterghem <benjamin.v.renterghem@gmail.com>
2016-11-11 12:44:07 +0100

    changed screen size

--- desktop/src/be/howest/junglewars/desktop/DesktopLauncher.java
+++ desktop/src/be/howest/junglewars/desktop/DesktopLauncher.java
@@ -4,14 +4,24 @@ import be.howest.junglewars.game.*;
 import com.badlogic.gdx.*;
 import com.badlogic.gdx.backends.lwjgl.*;
 
+import java.awt.*;
+
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.title = "Jungle Wars: The Revenge of Harambe";
-        config.width = 1200;
+
+//        Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
+//        config.width = (int) screensize.getWidth();
+//        config.height = (int) screensize.getHeight();
+//        config.fullscreen = true;
+        config.width = 1600;
         config.height = 900;
+
         config.resizable = false;
+
         config.useGL30 = false;
+
         new LwjglApplication(new JungleWarsGame(), config);
     }
 }
