65718e749a69ccb1099373c55618320e4070055f
Ward Pauwels <wardpauwels@hotmail.be>
2016-12-23 23:50:20 +0100

    commit

--- core/src/be/howest/junglewars/screens/MainMenuScreen.java
+++ core/src/be/howest/junglewars/screens/MainMenuScreen.java
@@ -317,6 +317,7 @@ public class MainMenuScreen extends Stage implements Screen {
                         game.setScreen( new GameScreen( game, 1, difficulty) );
                         break;
                     case "multi":
+                        dispose();
                         game.setPlayer(new PlayerEntity("harambe",1));
                         game.setScreen(new GameScreen(game,2,1, difficulty));
                         break;
